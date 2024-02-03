9cf9104edd01713684b3dd2ea890ff620beb1596
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-03 16:56:08 +0600

    2-nd-commit

---
+++ task-1.js
@@ -0,0 +1,16 @@
+// function multiplyNumber(a, b, c, d) {
+//   const result= a * b * c * d;
+//   return result;
+// }
+// //Example usage:
+// const porduct = multiplyNumber(2, 3, 4, 5);
+// console.log(porduct);
+
+
+function multiplyNumber(a, b, c, d, ){
+ const result = a * b * c * d;
+ return result 
+}
+const name = multiplyNumber(2, 3, 4, 5);
+console.log(name);
+
