6f81c92054e440b050ddffd3794d03ed60235d17
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-03 21:14:30 +0600

    updated

---
+++ parr.js
@@ -0,0 +1,13 @@
+function inchToFeet(inch) {
+  const feet = inch / 12;
+  return feet;
+}
+const rakibKhan = inchToFeet(75);
+console.log(rakibKhan);
+
+//
+
+function inchToFeet2(inch) {
+  const feetFaction = inchToFeet2 / 12;
+  return feetFaction
+}
