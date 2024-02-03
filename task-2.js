a2223fa3cc68c7d6fef74ef617e0e9eadd5ca02e
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-03 17:26:17 +0600

    g

--- task-2.js
+++ task-2.js
@@ -1,13 +1,28 @@
+// function processNumber(number){
+//   if(number % 2 === 0){
+//     return number / 2;
+// }else {
+//   return number * 2; 
+// }
+// }
+
+// const resultForEven = processNumber(8);
+// console.log(resultForEven);
+
+// const resultForOdd = processNumber(5);
+// console.log(resultForOdd);
+
+//**** */
 function processNumber(number){
   if(number % 2 === 0){
-    return number / 2;
-}else {
-  return number * 2; 
-}
+    return number / 10;
+  } else{
+    return number * 5;
+  }
 }
 
-const resultForEven = processNumber(8);
+const resultForEven = processNumber(10);
 console.log(resultForEven);
 
-const resultForOdd = processNumber(5);
+const resultForOdd = processNumber(10);
 console.log(resultForOdd);
