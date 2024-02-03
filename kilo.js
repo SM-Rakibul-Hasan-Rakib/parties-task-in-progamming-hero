c784c78bfd154e1d82033353de9d704a0619d1c7
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-03 21:32:26 +0600

    updated

---
+++ kilo.js
@@ -0,0 +1,10 @@
+// function kilometer (mile){
+//   const miles = kilometer * 0.621371;
+//   return miles;
+// }
+// const rakib = kilometer(2);
+// console.log(rakib)
+function KiloMeterMiles(kilo){
+  const mile = kilo * 0.621371;
+  return mile;
+}
