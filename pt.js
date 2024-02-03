aed65cd0799794fccc7feaf2ed6eb98349d16858
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-03 21:23:26 +0600

    funcation-updated

---
+++ pt.js
@@ -0,0 +1,15 @@
+function milsToKm(kilometer) {
+  const km = kilometer * 1.609344;
+  return km;
+}
+// const mainResult = milsToKm(80);
+// console.log(mainResult);
+
+
+function mileKilometer (mile){
+  const kilo = mile * 1.609344;
+  return kilo; 
+}
+
+const rakib = mileKilometer(10);
+console.log(rakib);
