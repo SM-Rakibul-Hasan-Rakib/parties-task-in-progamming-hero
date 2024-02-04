dbaf578953c5d5dd9772816b25043763644e4839
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-04 21:37:49 +0600

    hope

---
+++ a.js
@@ -0,0 +1,11 @@
+const disha = 56;
+const salman = 95;
+
+if(disha  > salman) {
+  console.log('Disha will get the strawbeery');
+} else{
+  console.log('Salman will get the strawbeery');
+}
+
+//inside a function
+function 
