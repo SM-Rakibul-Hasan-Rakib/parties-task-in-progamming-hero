85b8ae8dbc2bbb85331e28d378a92e52c5f2f37d
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-04 22:45:30 +0600

    discount

--- min.js
+++ min.js
@@ -1,9 +1,9 @@
-const heights = [65, 66, 2, 68, 72, , 78, 60, 65 , 100];
+const heights = [65, 66, 2, 68, 72, , 78, 60, 65, 100];
 
 function getMin(numbers) {
   let min = numbers[0];
-  for (const num of numbers){
-    if(num < min){
+  for (const num of numbers) {
+    if (num < min) {
       min = num;
     }
   }
@@ -11,4 +11,4 @@ function getMin(numbers) {
 }
 
 const min = getMin(heights);
-console.log(min);
+fconsole.log(min);
