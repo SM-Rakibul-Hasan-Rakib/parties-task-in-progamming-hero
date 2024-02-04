5fb652b9fc658b6c5bb0528eeaad654f8197a83b
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-04 16:33:29 +0600

    g

---
+++ bri.js
@@ -0,0 +1,14 @@
+const name = ['rakib', 'bhakib', 'chakib', 'nakib', 'jokib', 'tokib'];
+const number = [2, 3, 4, 5, 6, 7, ];
+
+
+function  briyanikhor(array){
+  const unique = [];
+  for(cost item of array){
+    if()
+  }
+}
+
+
+const uniqueArray =  noDoublicate(briyanikhor);
+console.log(uniqueArray);
---
+++ math.js
@@ -0,0 +1,18 @@
+const min =Math.min(45, 1, 65, 99, 123);
+console.log(min);
+
+const max =Math.max(455555, 65, 76, 88, 2, 8, 7);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.round(88));
+console.log(Math.abs(9-90));
+console.log(Math.SQRT2);
+console.log(Math.cos(80));
+console.log(Math.floor(7.9));
+console.log(Math.ceil(7.9));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+const rand = Math.round(math.random(10));
+console.log(rand);
