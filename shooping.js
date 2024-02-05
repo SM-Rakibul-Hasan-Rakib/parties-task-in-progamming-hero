5e72899e8301d3d8a2d83837bbb94de10cdabf1d
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-05 21:02:16 +0600

    g

--- shooping.js
+++ shooping.js
@@ -1,6 +1,19 @@
 const products = [
-  { name: "Shampo", price: 300 },
-  { name: "Chiruni", price: 100 },
-  { name: "Shirt", price: 700 },
-  { name: "pant", price: 1200 },
-];
+  { name: "Shampo", price: 300, quantity:2},
+  { name: "Chiruni", price: 100, quantity:3},
+  { name: "Shirt", price: 700 ,quantity:5},
+  { name: "pant", price: 1200 , quantity:4},
+]; 
+
+
+function cardTotal(products){
+  const total = 0;
+  for(const product of products){
+    const productCost = product.price * product.quantity;
+    total = total + productCost;
+  }
+  return total;
+}
+
+const ShoopingCost = cardTotal(products);
+console.log(ShoopingCost);
