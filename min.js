d1d2be9e1285d2522ccd686df8e9681435138dbf
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-05 19:35:32 +0600

    g

--- min.js
+++ min.js
@@ -1,6 +1,6 @@
 const heights = [65, 66, 2, 68, 72, , 78, 60, 65, 100];
 
-function getMin(numbers) {
+function getMin(  ) {
   let min = numbers[0];
   for (const num of numbers) {
     if (num < min) {
---
+++ wood.js
@@ -0,0 +1,58 @@
+// function woodQuality(chairQuality, tableQuality, bedQuality){
+//   const parChairwood = 3;
+//   const parTablewood = 10;
+//   const parBedwood = 50;
+
+//   const chaitTotalWood  = chairQuality * parChairwood;
+//   const tableTotalWood = tableQuality * parTablewood;
+//   const  bedtotalWood = tableQuality * parTablewood;
+
+//   const totalWood =bedQuality + tableQuality +chairQuality;
+
+//   return totalWood;
+// }
+
+// const wood = woodQuality(0, 0, 1);
+// console.log('wood needed', wood);
+
+//shart price- 500
+//pant-price-500
+//juta price-1000
+//
+
+// function dreesQuality(shartQuality, pantQuality, jutaQuality) {
+//   const parShart = 500;
+//   const parPant = 500;
+//   const parJuta = 1000;
+
+//   const totalShartPrice = shartQuality * parShart;
+//   const totalPantPrice = pantQuality * parPant;
+//   const totalJutaPrice = jutaQuality * parJuta;
+
+//   const totalPrice = totalShartPrice + totalPantPrice + totalJutaPrice;
+//   return totalPrice;
+// }
+
+// const total = dreesQuality(2, 2, 1);
+// console.log(total);
+
+
+//college fee-500;
+//bus bhara- 1000;
+//basa-bhara-5000;;
+function totalFree(collegeFee, busFee, basaFee ){
+  const  parCollege = 5000;
+  const parbasaBhara= 6000;
+  const parbusBhara = 5000;
+
+  const totalCollege = collegeFee * parCollege;
+  const totalbasaBhara = basaFee * parbasaBhara;
+  const totalbusBhara = busFee * parbusBhara;
+
+  const totalAmount = totalCollege + totalbasaBhara + totalbusBhara;
+
+  return totalAmount;
+}
+
+const total = totalFree(4,6j,1);
+console.log('par month totat amount fot money:', total);
